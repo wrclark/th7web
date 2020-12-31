fdf5b463a6841338ceabab18c550246564560879
William Clark <williamclark@protonmail.com>
2020-12-31 15:47:29 +0000

    first commit

---
+++ th7.js
@@ -0,0 +1,23 @@
+
+function updateList() {
+
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			document.getElementById("update-response").innerHTML = this.responseText;
+			updateListItem(this.responseText);
+		}
+
+	};
+
+	xhttp.open("GET", "test_data.json", true);
+	xhttp.send();
+}
+
+function updateListItem(data) {
+	console.log(data);
+	var x = JSON.parse(data);
+	for(i in x) {
+		document.getElementById("li".concat(parseInt(i)+1)).innerHTML = x[i].value;
+	}
+}
