c2b3107c7dde0818253504aae3dc8246137a927c
William Clark <williamclark@protonmail.com>
2021-01-01 17:04:37 +0000

    test

--- th7.js
+++ th7.js
@@ -19,7 +19,14 @@ function updateListItem(data) {
 	console.log(data);
 	var x = JSON.parse(data);
 	for(i in x) {
-		document.getElementById("li".concat(parseInt(i)+1)).innerHTML = x[i].value;
+		//document.getElementById("li".concat(parseInt(i)+1)).innerHTML = x[i].value;
+		var cells = document.getElementById("channel1").getElementsByTagName('td');
+		cells[0].innerHTML = parseInt(x[i].id) + 1;
+		cells[1].innerHTML = x[i].tempc;
+		cells[2].innerHTML = x[i].type;
+		cells[3].innerHTML = x[i].offset;
+		cells[4].innerHTML = x[i].gain;
+		cells[5].innerHTML = x[i].value;
 	}
 }
 
@@ -27,10 +34,12 @@ function updatePCB() {
 var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
                 if (this.readyState == 4 && this.status == 200) {
-			//document.getElementById("update-response").innerHTML = this.responseText;
-                        //updateListItem(this.responseText);
-                        //updatePCB()
-			console.log(this.responseText);
+					var x = JSON.parse(this.responseText);
+					document.getElementById("pcb_temp").innerHTML = x['pcb_temp'];
+					document.getElementById("pivdd").innerHTML = x['pivdd'];
+					document.getElementById("pivadj").innerHTML = x['pivadj'];
+					document.getElementById("pivref").innerHTML = x['pivref'];
+					
                 }
 
         };
